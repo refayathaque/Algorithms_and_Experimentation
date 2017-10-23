09b3a2d1737dcdab09aaef90140641a58c858c3a
Refayat <refayathaque@gmail.com>
2017-10-23 08:43:17 -0400

    did .js .py fizzbuzzes

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,4 @@
-const fizzbuzz19 = () => {
+const fizzBuzz1 = () => {
     for (i = 1; i < 101; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
             console.log('FizzBuzz');
@@ -14,26 +14,26 @@ const fizzbuzz19 = () => {
         }
     }
 }
-fizzbuzz19()
-console.log('--------10/22/17-------------');
+fizzBuzz1()
+console.log('__________10/23/17__________');
 
-const fizzbuzz20 = () => {
-    let i = 1
+const fizzBuzz2 = () => {
+    let = 1;
     while (i < 101) {
-        if (i % 3 == 0 && i % 5 == 0) {
+        if (i % 3 === 0 && i % 5 === 0) {
             console.log('FizzBuzz');
         }
-        else if (i % 3 == 0) {
+        if (i % 3 === 0) {
             console.log('Fizz');
         }
-        else if (i % 5 == 0) {
+        if (i % 5 === 0) {
             console.log('Buzz');
         }
         else {
             console.log(i);
         }
-        i++
+        i++;
     }
 }
-fizzbuzz20()
-console.log('--------10/22/17-------------');
+fizzBuzz2()
+console.log('__________10/23/17__________');
---
+++ fizzbuzz.py
@@ -0,0 +1,27 @@
+def fizzBuzz1():
+    for i in range (1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print 'FizzBuzz'
+        elif i % 3 == 0:
+            print 'Fizz'
+        elif i % 5 == 0:
+            print 'Buzz'
+        else:
+            print i
+fizzBuzz1()
+print '__________10/23/17__________'
+
+def fizzBuzz2():
+    i = 1
+    while i < 101:
+        if i % 3 == 0 and i % 5 ==0:
+            print 'FizzBuzz'
+        elif i % 3 == 0:
+            print 'Fizz'
+        elif i % 5 == 0:
+            print 'Buzz'
+        else:
+            print i
+        i += 1
+fizzBuzz2()
+print '__________10/23/17__________'
