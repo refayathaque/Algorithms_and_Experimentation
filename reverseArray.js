f3da22c4a8b93d4fbf4c1782196c5d77d75169cf
Refayat <refayathaque@gmail.com>
2017-10-23 09:08:55 -0400

    did reversearrays in .py and .js

---
+++ reverseArray.js
@@ -0,0 +1,24 @@
+const reverseArray1 = (arr) => {
+    let temp = 0
+    for (left = 0, right = (arr.length - 1); left < right; left++, right--) {
+        temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+    }
+    return arr;
+}
+q = [1, 2, 3, 4, 5]
+console.log(reverseArray1(q));
+console.log('__________10/23/17__________');
+
+const reverseArray2 = (arr) => {
+    let temp = 0
+    for (i = 0; i < arr.length / 2; i++) {
+        temp = arr[i];
+        arr[i] = arr[(arr.length - 1) - i];
+        arr[(arr.length - 1) - i] = temp;
+    }
+    return arr;
+}
+console.log(reverseArray2(q));
+console.log('__________10/23/17__________');
---
+++ reverseArray.py
@@ -0,0 +1,24 @@
+def reverseArray1(list):
+    for i in range (0, len(list) / 2):
+        temp = list[i]
+        list[i] = list[(len(list) - 1) - i]
+        list[(len(list) - 1) - i] = temp
+    return list
+q = [1, 2, 3, 4, 5]
+print reverseArray1(q)
+print '__________10/23/17__________'
+
+def reverseArray2(list):
+    left = 0
+    right = len(list) - 1
+    while left < right:
+        temp = list[left]
+        list[left] = list[right]
+        list[right] = temp
+        left += 1
+        right -= 1
+    return list
+print reverseArray2(q)
+print '__________10/23/17__________'
+
+####################################
