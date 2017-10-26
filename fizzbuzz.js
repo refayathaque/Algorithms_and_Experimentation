10268f101b3c84437bf603f7f0c6ff09e6ee8c98
Refayat <refayathaque@gmail.com>
2017-10-26 13:24:50 -0400

    .js fizzbuzz practice

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -18,7 +18,7 @@ fizzBuzz1()
 console.log('__________10/23/17__________');
 
 const fizzBuzz2 = () => {
-    let = 1;
+    let i = 1;
     while (i < 101) {
         if (i % 3 === 0 && i % 5 === 0) {
             console.log('FizzBuzz');
@@ -39,3 +39,43 @@ fizzBuzz2()
 console.log('__________10/23/17__________');
 
 ///////////////////////////////////////////
+
+const fizzBuzz3 = () => {
+    for (i = 100; i > 0; i--) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('FizzBuzz');
+        }
+        else if (i % 3 === 0) {
+            console.log('Fizz');
+        }
+        else if (i % 5 == 0) {
+            console.log('Buzz');
+        }
+        else {
+            console.log(i);
+        }
+    }
+}
+fizzBuzz3()
+console.log('__________10/26/17__________');
+
+const fizzBuzz4 = () => {
+    let i = 100;
+    while (i > 0) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('FizzBuzz');
+        }
+        else if (i % 3 === 0) {
+            console.log('Fizz');
+        }
+        else if (i % 5 === 0) {
+            console.log('Buzz');
+        }
+        else {
+            console.log(i);
+        }
+        i--;
+    }
+}
+fizzBuzz4()
+console.log('__________10/26/17__________');
