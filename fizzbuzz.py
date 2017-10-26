27bd4abb04fdd655eebf3b606fdf0bc1fa544708
Refayat <refayathaque@gmail.com>
2017-10-26 13:38:14 -0400

    .py fizzbuzz practice

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -14,7 +14,7 @@ print '__________10/23/17__________'
 def fizzBuzz2():
     i = 1
     while i < 101:
-        if i % 3 == 0 and i % 5 ==0:
+        if i % 3 == 0 and i % 5 == 0:
             print 'FizzBuzz'
         elif i % 3 == 0:
             print 'Fizz'
@@ -27,3 +27,31 @@ fizzBuzz2()
 print '__________10/23/17__________'
 
 ####################################
+
+def fizzBuzz3():
+    for i in range (100, 0, -1): # Third argument is for decrementing value, can also decrement by more than 1 IE '-2'
+        if i % 3 == 0 and i % 5 == 0:
+            print 'FizzBuzz'
+        elif i % 3 == 0:
+            print 'Fizz'
+        elif i % 5 == 0:
+            print 'Buzz'
+        else:
+            print i
+fizzBuzz3()
+print '__________10/26/17__________'
+
+def fizzBuzz4():
+    i = 100
+    while i > 0:
+        if i % 3 == 0 and i % 5 == 0:
+            print 'FizzBuzz'
+        elif i % 3 == 0:
+            print 'Fizz'
+        elif i % 5 == 0:
+            print 'Buzz'
+        else:
+            print i
+        i -= 1
+fizzBuzz4()
+print '__________10/26/17__________'
