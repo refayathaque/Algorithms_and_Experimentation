6b9921805481dd6a13fca1877687ef184bfb28f9
Refayat <refayathaque@gmail.com>
2017-10-26 14:07:13 -0400

    .py reverseArray practice

--- reverseArray.py
+++ reverseArray.py
@@ -22,3 +22,27 @@ print reverseArray2(q)
 print '__________10/23/17__________'
 
 ####################################
+
+w = ['a', 'b', 'c', 'd']
+
+def reverseArray3(list):
+    for i in range (0, len(list) / 2):
+        temp = list[i]
+        list[i] = list[(len(list) - 1) - i]
+        list[(len(list) - 1) - i] = temp
+    return list
+print reverseArray3(w)
+print '__________10/26/17__________'
+
+def reverseArray4(list):
+    left = 0
+    right = len(list) - 1
+    while left < right:
+        temp = list[left]
+        list[left] = list[right]
+        list[right] = temp
+        left += 1
+        right -= 1
+    return list
+print reverseArray4(w)
+print '__________10/26/17__________'
