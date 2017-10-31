1f2656e5c300b4d992c768f861f3b268bb5ec43f
Refayat <refayathaque@gmail.com>
2017-10-31 09:01:35 -0400

    .py reversearray

--- reverseArray.py
+++ reverseArray.py
@@ -48,3 +48,27 @@ print reverseArray4(w)
 print '__________10/26/17__________'
 
 ####################################
+
+e = ['tom', 'dick', 'harry']
+
+def reverseArray5(list):
+    for i in range (0, len(list) / 2):
+        temp = list[i]
+        list[i] = list[(len(list) - 1) - i]
+        list[(len(list) - 1) - i] = temp
+    return list
+print reverseArray5(e)
+print '__________10/31/17__________'
+
+def reverseArray6(list):
+    left = 0
+    right = len(list) - 1
+    while left < right:
+        temp = list[left]
+        list[left] = list[right]
+        list[right] = temp
+        left += 1
+        right -= 1
+    return list
+print reverseArray6(e)
+print '__________10/31/17__________'
