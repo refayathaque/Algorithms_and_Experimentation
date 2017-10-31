7f5ce002dc9c13d7a43d2bd68df08c6d5381b708
Refayat <refayathaque@gmail.com>
2017-10-31 08:52:38 -0400

    .js reversearray

--- reverseArray.js
+++ reverseArray.js
@@ -52,3 +52,31 @@ console.log(reverseArray4(w));
 console.log('__________10/26/17__________');
 
 ///////////////////////////////////////////
+
+let e = [1, 3, 5, 7, 9]
+
+const reverseArray5 = (arr) => {
+    let temp = 0;
+    for (left = 0, right = arr.length - 1; left < right; left++, right--) {
+        temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+    }
+    return arr;
+}
+console.log(reverseArray5(e));
+console.log('__________10/31/17__________');
+
+const reverseArray6 = (arr) => {
+    let temp = 0;
+    for (i = 0; i < arr.length / 2; i++) {
+        temp = arr[i];
+        arr[i] = arr[(arr.length - 1) - i];
+        arr[(arr.length - 1) - i] = temp;
+    }
+    return arr;
+}
+console.log(reverseArray6(e));
+console.log('__________10/31/17__________');
+
+///////////////////////////////////////////
