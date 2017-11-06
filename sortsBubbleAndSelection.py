4b9084ed8c27fd85e7385b2e8415e0fa3ed270f0
Refayat <refayathaque@gmail.com>
2017-11-06 07:26:17 -0500

    old sorting algorithms were actually incorrect, and were not identified as incorrect because of mutable nature of lists in python

--- sortsBubbleAndSelection.py
+++ sortsBubbleAndSelection.py
@@ -22,6 +22,8 @@ def bubbleSortDesc(list):
 print bubbleSortDesc(q)
 print '__________10/26/17__________'
 
+w = [1, 9, 6, 3, 8, 9, 7, 6, 2, 3, 4, 6]
+
 def bubbleSortDescRec(list):
     if len(list) == 0:
         return 'List is empty'
@@ -31,35 +33,36 @@ def bubbleSortDescRec(list):
     for i in range (0, len(list) - 1):
         if list[i] < list[i + 1]:
             temp = 0
-            temp = list[i]
-            list[i] = list[i + 1]
-            list[i + 1] = temp
+            temp = list[i + 1]
+            list[i + 1] = list[i]
+            list[i] = temp
             swapped = True
     if swapped == True:
-        bubbleSortDescRec
+        bubbleSortDescRec(list)
     return list
-print bubbleSortDescRec(q)
+print bubbleSortDescRec(w)
 print '__________10/26/17__________'
+
+r = [1, 9, 6, 3, 8, 9, 7, 6, 2, 3, 4, 6]
 
 def selectionSortDesc(list):
     if len(list) == 0:
         return 'List is empty'
     if len(list) == 1:
         return list
-    for i in range (0, len(list) - 1):
-        for y in range (i + 1, len(list) - 1):
+    for i in range (0, len(list)):
+        for y in range (i + 1, len(list)):
             if list[y] > list[i]:
-                temp = 0
                 temp = list[y]
-                list[y] = list[y + 1]
-                list[y + 1] = temp
+                list[y] = list[i]
+                list[i] = temp
     return list
-print selectionSortDesc(q)
+print selectionSortDesc(r)
 print '__________10/26/17__________'
 
 ####################################
 
-w = [11, 33, 77, 44, 66, 99, 22, 88]
+e = [11, 33, 77, 44, 66, 99, 22, 88]
 
 def bubbleSortAsc(list):
     if len(list) == 0:
@@ -79,7 +82,26 @@ def bubbleSortAsc(list):
         if sorted_count == len(list) - 1:
             sorted_flag = True
     return list
-print bubbleSortAsc(w)
+print bubbleSortAsc(e)
 print '__________10/31/17__________'
+
+####################################
+
+t = ['d', 'a', 'c', 'b']
+
+def selectionSortAsc(list):
+    if len(list) == 0:
+        return 'List is empty'
+    if len(list) == 1:
+        return list
+    for i in range (0, len(list)):
+        for y in range (i + 1, len(list)):
+            if list[y] < list[i]:
+                temp = list[y]
+                list[y] = list[i]
+                list[i] = temp
+    return list
+print selectionSortAsc(t)
+print '__________11/06/17__________'
 
 ####################################
