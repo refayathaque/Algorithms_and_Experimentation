cae145f452b97bc02ea2502963640d1c5c83aa26
Refayat <refayathaque@gmail.com>
2017-11-06 07:38:50 -0500

    git good

--- sortsBubbleAndSelection.js
+++ sortsBubbleAndSelection.js
@@ -27,6 +27,8 @@ const bubbleSortAsc = (arr) => {
 console.log(bubbleSortAsc(q));
 console.log('__________10/26/17__________');
 
+w = ['Tom', 'Dick', 'Harry']
+
 const bubbleSortAscRec = (arr) => {
     if (arr.length === 0) {
         return 'Array is empty'
@@ -46,8 +48,10 @@ const bubbleSortAscRec = (arr) => {
     }
     return arr;
 }
-console.log(bubbleSortAscRec(q));
+console.log(bubbleSortAscRec(w));
 console.log('__________10/26/17__________');
+
+e = ['Tom', 'Dick', 'Harry']
 
 const selectionSortAsc = (arr) => {
     if (arr.length === 0) {
@@ -65,12 +69,12 @@ const selectionSortAsc = (arr) => {
     }
     return arr;
 }
-console.log(selectionSortAsc(q));
+console.log(selectionSortAsc(e));
 console.log('__________10/26/17__________');
 
 ///////////////////////////////////////////
 
-w = [11, 33, 77, 44, 66, 99, 22, 88]
+r = [11, 33, 77, 44, 66, 99, 22, 88]
 
 const bubbleSortDesc = (arr) => {
     if (arr.length === 0) {
@@ -98,7 +102,36 @@ const bubbleSortDesc = (arr) => {
     }
     return arr;
 }
-console.log(bubbleSortDesc(w));
+console.log(bubbleSortDesc(r));
 console.log('__________10/31/17__________');
+
+///////////////////////////////////////////
+
+t = [76, -3, 5, 12, -67, 101]
+
+const bubbleSortDescRec = (arr) => {
+    if (arr.length === 0) {
+        return 'Array is empty';
+    }
+    if (arr.length === 1) {
+        return arr;
+    }
+    let swapped_flag = false;
+    for (i = 0; i < arr.length; i++) {
+        let temp = 0
+        if (arr[i] < arr[i + 1]) {
+            temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+            swapped_flag = true
+        }
+        if (swapped_flag) {
+            bubbleSortDescRec;
+        }
+        return arr;
+    }
+}
+console.log(bubbleSortDesc(t));
+console.log('__________11/06/17__________');
 
 ///////////////////////////////////////////
